3ac904e3c9542b5ac612004d260f88fd8f8c24f1
jason13official <jason13official@gmail.com>
2026-04-19 15:23:56 -0400

    cleanup living renderer mixin

--- src/main/java/io/github/jason13official/zodiac_gems/mixin/LivingEntityRendererMixin.java
+++ src/main/java/io/github/jason13official/zodiac_gems/mixin/LivingEntityRendererMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,34 +25,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> implements RenderLayerParent<T, M> {
 
   @Shadow
-  protected abstract boolean isBodyVisible(T pLivingEntity);
-
-  @Shadow
-  @Nullable
-  protected abstract RenderType getRenderType(T pLivingEntity, boolean pBodyVisible, boolean pTranslucent, boolean pGlowing);
-
-  @Shadow
-  protected abstract float getWhiteOverlayProgress(T pLivingEntity, float pPartialTicks);
-
-  @Shadow
   public static int getOverlayCoords(LivingEntity pLivingEntity, float pU) {
     throw new UnsupportedOperationException("Implemented via mixin");
   }
-
-  @Shadow
-  protected M model;
 
   /// dummy
   public LivingEntityRendererMixin(Context pContext) {
     super(pContext);
   }
 
+  @SuppressWarnings("unsafe")
   @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V"))
   private void zodiac_gems$render(T pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, CallbackInfo ci) {
-
-//    if (!pEntity.getTags().contains("Darkness")) {
-//      return;
-//    }
 
     LivingEntityRenderer<T, M> self = (LivingEntityRenderer<T, M>) (Object) this;
 
@@ -67,10 +52,13 @@ public abstract class LivingEntityRendererMixin<T extends LivingEntity, M extend
 
     pPoseStack.pushPose();
     ResourceLocation resourcelocation = instance.getTextureLocation(pEntity);
+
+    // cool effect but doesn't render through walls
     // RenderType rendertype = RenderType.entityTranslucent(resourcelocation, true);
+
     RenderType rendertype = RenderType.outline(resourcelocation);
     VertexConsumer vertexconsumer = pBuffer.getBuffer(rendertype);
-    int i = getOverlayCoords(pEntity, 0.0f);
+    int i = OverlayTexture.NO_OVERLAY;
     pPoseStack.scale(1.1f, 1.1f, 1.1f); // temporary scaling for testing
     instance.getModel().renderToBuffer(pPoseStack, vertexconsumer, pPackedLight, i, 0x26131313);
     pPoseStack.popPose();
