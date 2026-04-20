9a56163f4accb89d2d4f10783b4b705779385eab
jason13official <jason13official@gmail.com>
2026-04-19 22:47:23 -0400

    fix slots

--- src/main/java/io/github/jason13official/zodiac_gems/mixin/InventoryMenuMixin.java
+++ src/main/java/io/github/jason13official/zodiac_gems/mixin/InventoryMenuMixin.java
@@ -2,9 +2,6 @@ package io.github.jason13official.zodiac_gems.mixin;
 
 import io.github.jason13official.zodiac_gems.accessor.AdditionalInventory;
 import io.github.jason13official.zodiac_gems.impl.common.registry.ModItems;
-import java.util.concurrent.atomic.AtomicBoolean;
-import net.minecraft.core.NonNullList;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.InventoryMenu;
@@ -22,7 +19,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InventoryMenu.class)
 public abstract class InventoryMenuMixin extends RecipeBookMenu<CraftingInput, CraftingRecipe> {
 
-  /// dummy
   public InventoryMenuMixin(MenuType<?> pMenuType, int pContainerId) {
     super(pMenuType, pContainerId);
   }
@@ -30,31 +26,20 @@ public abstract class InventoryMenuMixin extends RecipeBookMenu<CraftingInput, C
   @Inject(at = @At("RETURN"), method = "<init>")
   private void zodiac$constructor(Inventory inventory, boolean pActive, Player pOwner, CallbackInfo ci) {
 
-    // the original inventory, being cast to a separate class. (re-exposes the same items)
-    // AdditionalInventory aegis = (AdditionalInventory) inventory;
-
-//    AtomicBoolean hasDiamond = new AtomicBoolean(false);
-//
-//    pOwner.getHandSlots().forEach(stack -> {
-//      if (stack.is(ModItems.DIAMOND)) {
-//        System.out.println("player is holding our diamond");
-//        hasDiamond.set(true);
-//      }
-//    });
-//
-//    if (!hasDiamond.get()) {
-//      return;
-//    }
-
-//    if (!(pOwner.getMainHandItem().is(ModItems.DIAMOND) || pOwner.getOffhandItem().is(ModItems.DIAMOND))) {
-//      return;
-//    }
-
     AdditionalInventory aegis = () -> ((AdditionalInventory) inventory).zodiac$getItems();
 
     for (int index = 0; index < 9; index++) {
+      this.addSlot(new Slot(aegis, index, 8 + index * 18, 142 + 32) {
+        @Override
+        public boolean mayPickup(Player player) {
+          return player.getMainHandItem().is(ModItems.DIAMOND) || player.getOffhandItem().is(ModItems.DIAMOND);
+        }
 
-      this.addSlot(new Slot(aegis, index, 8 + index * 18, 142 + 32));
+        @Override
+        public boolean mayPlace(ItemStack stack) {
+          return inventory.player.getMainHandItem().is(ModItems.DIAMOND) || inventory.player.getOffhandItem().is(ModItems.DIAMOND);
+        }
+      });
     }
   }
 }
